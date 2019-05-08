6271f2b8bc1d7726b78e973ab15c0f9b223d3852
Waldi6 <waldburger91@hotmail.com>
2019-05-08 23:30:00 +0200

    add view, viewmodel and view

---
+++ TestatNuGet/Model/LogentriesModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestatNuGet.Model
+{
+    public class LogentriesModel
+    {
+        public int Id { get; set; }
+        public string Pod { get; set; }
+        public string Location { get; set; }
+        public string Hostname { get; set; }
+        public int Severity { get; set; }
+        public string Timestamp { get; set; }
+        public string Message { get; set; }
+
+        public LogentriesModel(int id, string pod, string location, string hostname, int severity, string timestamp, string message)
+        {
+            this.Id = id;
+            this.Pod = pod;
+            this.Location = location;
+            this.Hostname = hostname;
+            this.Severity = severity;
+            this.Timestamp = timestamp;
+            this.Message = message;
+        }
+    }
+}
---
+++ TestatNuGet/ViewModel/LogentriesViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using TestatNuGet.Model;
+using MySql.Data.MySqlClient;
+
+namespace TestatNuGet.ViewModel
+{
+    public class LogentriesViewModel : INotifyPropertyChanged
+    {
+        public string ConnectionString { get; set; }
+        public ObservableCollection<LogentriesModel> Logentries { get; set; }
+
+        public LogentriesViewModel()
+        {
+            LoadLogentries();
+        }
+
+        public void LoadLogentries()
+        {
+            var con = new MySqlConnection("Server=localhost;Database=semesterarbeit;Uid=root;Pwd=password;");
+            var Logentries = new ObservableCollection<LogentriesModel>();
+            con.Open();
+            using (var cmd = con.CreateCommand())
+            {
+                cmd.CommandText = "SELECT id, pod, location, hostname, severity, timestamp, message FROM v_logentries";
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Logentries.Add(new LogentriesModel(
+                            1, "", "", "", 1, "", ""
+
+                            ));
+                    }
+                }
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
+
