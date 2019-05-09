4be7a5f6377a302fd7c58de00c72cd342a218dec
M. Waldburger <waldburger91@hotmail.com>
2019-05-09 18:55:27 +0200

    change datacontext definition into XAML code

--- TestatNuGet/View/TestatNuGetView.xaml.cs
+++ TestatNuGet/View/TestatNuGetView.xaml.cs
@@ -1,32 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using TestatNuGet.ViewModel;
 
 namespace TestatNuGet.View
 {
-    /// <summary>
-    /// Interaction logic for TestatNuGetView.xaml
-    /// </summary>
     public partial class TestatNuGetView : UserControl
     {
-        private LogentriesViewModel lvm;
         public TestatNuGetView()
         {
             InitializeComponent();
-            lvm = new LogentriesViewModel();
-            this.DataContext = lvm;
         }
 
         private void Button_load_Click(object sender, RoutedEventArgs e)
