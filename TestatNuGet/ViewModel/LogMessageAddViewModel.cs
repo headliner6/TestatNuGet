069935015509b5ddcaf4c994426ab6e9635da0a7
M. Waldburger <waldburger91@hotmail.com>
2019-05-14 11:11:46 +0200

    change method AddMessage to AddMessageAndValidation

--- TestatNuGet/ViewModel/LogMessageAddViewModel.cs
+++ TestatNuGet/ViewModel/LogMessageAddViewModel.cs
@@ -1,26 +1,23 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Input;
 using static TestatNuGet.ViewModel.NavigationViewModel;
-using TestatNuGet.ViewModel;
 
 namespace TestatNuGet.ViewModel
 {
     public class LogMessageAddViewModel
     {
         private readonly Action<object> navigate;
+        private bool _validationOk;
         public ICommand Navigate { get; set; }
         public ICommand NavigateBack { get; set; }
         public string POD { set; get; }
         public string Location { get; set; }
         public string Hostname { get; set; }
-        public int Severity { get; set; }
+        public string Severity { get; set; }
         public string Message { get; set; }
         public string ConnectionString { get; set; }
 
@@ -30,34 +27,66 @@ namespace TestatNuGet.ViewModel
             Navigate = new BaseCommand(OnNavigate);
             this.navigate = navigate;
        }
-        public void AddMessage()
+        public void AddMessageAndValidation()
         {
-            var connection = new MySqlConnection(ConnectionString);
-            try
+            if (string.IsNullOrEmpty(POD))
             {
-                connection.Open();
-                using (var cmd = connection.CreateCommand())
-                {
-                    cmd.CommandText = "LogMessageAdd";
-                    cmd.CommandType = CommandType.StoredProcedure;
-                    cmd.Parameters.AddWithValue("@i_pod", POD);
-                    cmd.Parameters.AddWithValue("@i_location", Location);
-                    cmd.Parameters.AddWithValue("@i_hostname", Hostname);
-                    cmd.Parameters.AddWithValue("@i_severity", Severity);
-                    cmd.Parameters.AddWithValue("@i_message", Message);
-                    cmd.ExecuteNonQuery();
-                }
-                connection.Close();
+                MessageBox.Show("POD darf nicht leer sein!");
             }
-            catch (Exception ex)
+            else if (string.IsNullOrEmpty(Location))
             {
-                MessageBox.Show("Folgender Fehler ist aufgetreten: " + ex.Message);
+                MessageBox.Show("Location darf nicht leer sein!");
+            }
+            else if (string.IsNullOrEmpty(Hostname))
+            {
+                MessageBox.Show("Hostname darf nicht leer sein!");
+            }
+            else if (string.IsNullOrEmpty(Severity))
+            {
+                MessageBox.Show("Severity darf nicht leer sein!");
+            }
+            else if (Regex.IsMatch(Severity, "[^0-9]"))
+            {
+                MessageBox.Show("Severity darf nur Zahlen enthalten!");
+                Severity = Severity.Remove(Severity.Length - 1);
+            }
+            else if (string.IsNullOrEmpty(Message))
+            {
+                MessageBox.Show("Message darf nicht leer sein!");
+            }
+            else
+            {
+                _validationOk = true;
+                var connection = new MySqlConnection(ConnectionString);
+                try
+                {
+                    connection.Open();
+                    using (var cmd = connection.CreateCommand())
+                    {
+                        cmd.CommandText = "LogMessageAdd";
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@i_pod", POD);
+                        cmd.Parameters.AddWithValue("@i_location", Location);
+                        cmd.Parameters.AddWithValue("@i_hostname", Hostname);
+                        cmd.Parameters.AddWithValue("@i_severity", Severity);
+                        cmd.Parameters.AddWithValue("@i_message", Message);
+                        cmd.ExecuteNonQuery();
+                    }
+                    connection.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Folgender Fehler ist aufgetreten: " + ex.Message);
+                }
             }
         }
         private void OnNavigate(object obj)
         {
-            AddMessage();
-            navigate.Invoke("LogentriesView");
+            AddMessageAndValidation();
+            if (_validationOk == true)
+            {
+                navigate.Invoke("LogentriesView");
+            }
         }
         private void OnNavigateBack(object obj)
         {
